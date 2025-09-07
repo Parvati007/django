ebce5d4cb7348798c54ba0a74b338305f98c47ea
Parvati007 <parvati000777@gmail.com>
2025-09-07 14:54:30 +0530

    updated

--- Hello/Hello/urls.py
+++ Hello/Hello/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,  include
+admin.site.site_header = "Olive Edge Admin"
+admin.site.site_title = "Olive Edge Admin Portal"
+admin.site.index_title = "Welcome to Olive Edge Researcher Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
