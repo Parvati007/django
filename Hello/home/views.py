5704ec65158b829be0b4384a0490ad308133d5f1
Parvati007 <parvati000777@gmail.com>
2025-09-08 15:14:07 +0530

    updated

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -9,10 +9,13 @@ def index(request):
     return render(request, 'index.html', context)
 
 def about(request):
-    return HttpResponse('this is about page')
+    #return HttpResponse('this is about page')
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse('this is contact page')
+    #return HttpResponse('this is contact page')
+    return render(request, 'contact.html')
 
 def services(request):
-    return HttpResponse('this is services page')
+    #return HttpResponse('this is services page')
+    return render(request, 'services.html')
