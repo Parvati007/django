449def28933f590ca54f4c246da598a991f5b0d0
Parvati007 <parvati000777@gmail.com>
2025-09-22 19:51:01 +0530

    updated

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,6 +9,7 @@ def index(request):
         "variable1" : "parvati",
         "variable2" : "lakshmi"
     }
+    
     return render(request, 'index.html', context)
 
 def about(request):
@@ -22,6 +24,7 @@ def contact(request):
         desc=request.POST.get('desc')
         contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
+        messages.success(request, "Your message has been sent!")
     return render(request, 'contact.html')
 
 def services(request):
